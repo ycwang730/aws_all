5404638899c6210cfb152b59a89f755e53671bf6
ycwang730 <yinchieh89@gmail.com>
2017-06-20 15:59:32 -0700

    change delete_new_instance.py

--- delete_new_instance.py
+++ delete_new_instance.py
@@ -4,28 +4,31 @@ import boto
 import boto.vpc
 import boto.vpc.vpc
 
+def main():
+ instance_to_delete=sys.argv[1]
+ vol_to_delete=sys.argv[2]
+ snapshot_to_delete=sys.argv[3]
+ print(sys.argv)
 
-instance_to_delete=sys.argv[1]
-vol_to_delete=sys.argv[2]
-snapshot_to_delete=sys.argv[3]
-
-ec2=boto.connect_ec2()
-ec2=boto.ec2.connect_to_region('us-west-1')
+ ec2=boto.connect_ec2()
+ ec2=boto.ec2.connect_to_region('us-west-1')
 #vpccon = boto.vpc.connect_to_region('us-west-1')
 
 #terminate instance
-print "Terminate the instance"
-ec2.terminate_instances(instance_ids=instance_to_delete)
-time.sleep(60)
+ print "Terminate the instance"
+ ec2.terminate_instances(instance_ids=instance_to_delete)
+ time.sleep(60)
 
 #print "Detach the volume"
 #ec2.detach_volume(vol_to_delete,instance_id=instance_to_delete)
 #time.sleep(5)
 
-print "Delete the volume"
-ec2.delete_volume(vol_to_delete)
-time.sleep(5)
+ print "Delete the volume"
+ ec2.delete_volume(vol_to_delete)
+ time.sleep(5)
 
-print "Delete the snapshot"
-ec2.delete_snapshot(snapshot_to_delete)
+ print "Delete the snapshot"
+ ec2.delete_snapshot(snapshot_to_delete)
 
+if __name__ == "__main__":
+ main()
